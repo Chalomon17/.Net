b70196652ab8c7270c5dc09fd40e0f35591636ca
Chalo Mata <mauriciomata1997@hotmail.com>
2024-05-16 00:02:39 -0600

    Se crearon las entidades en la carpeta Entities de Domain

---
+++ Traking.Vehiculos/src/Traking.Vehiculos.Domain/Entities/Propietario/PropietarioEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traking.Vehiculos.Domain.Entities.Propietario
+{
+    #region Entidad Propietario
+
+    public class PropietarioEntity
+    {
+        // Id del propietario
+        public Guid IdPropietario { get; set; }
+
+        // Nombres del propietario
+        public string Nombres { get; set; }
+
+        // Apellido paterno del propietario
+        public string ApellidoPaterno { get; set; }
+
+        // Apellido materno del propietario
+        public string ApellidoMaterno { get; set; }
+
+        // Dirección del propietario
+        public string Direccion { get; set; }
+
+        // Teléfono del propietario
+        public int Telefono { get; set; }
+
+        // Fecha de cuando se creó el registro (Campo de auditoria)
+        public DateTime FechaRegistro { get; set; }
+
+        // Fecha de cuando se actualizó el registro (Campo de auditoria)
+        public DateTime? FechaActualizacion { get; set; }
+
+        // Usuario quien modificó el registro (Campo de auditoria)
+        public string UsuarioModificacion { get; set; }
+    }
+
+    #endregion
+}
---
+++ Traking.Vehiculos/src/Traking.Vehiculos.Domain/Entities/Vehiculo/VehiculoEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traking.Vehiculos.Domain.Entities.Vehiculo
+{
+    #region Entidad Vehiculo
+
+    public class VehiculoEntity
+    {
+        // Id del vehículo
+        public Guid IdVehiculo { get; set; }
+   
+        // Marca del vehículo
+        public string Marca { get; set; }
+        
+        // SubMarca del vehículo
+        public string SubMarca { get; set; }
+        
+        // Modelo del vehículo
+        public int Modelo { get; set; }
+        
+        // Id foranea del tipo del vehículo
+        public Guid IdTipoVehiculo { get; set; }
+
+        // Fecha de cuando se creó el registro (Campo de auditoria)
+        public DateTime FechaRegistro { get; set; }
+
+        // Fecha de cuando se actualizó el registro (Campo de auditoria)
+        public DateTime? FechaActualizacion { get; set; }
+
+        // Usuario quien modificó el registro (Campo de auditoria)
+        public string UsuarioModificacion { get; set; }
+
+    }
+
+    #endregion
+}
