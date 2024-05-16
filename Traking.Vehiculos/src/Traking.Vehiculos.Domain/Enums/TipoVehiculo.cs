748bd95e41cca1b983b1fa413e97ad03af0f5aa9
Chalo Mata <mauriciomata1997@hotmail.com>
2024-05-16 00:13:39 -0600

    Se agrego enumaracion para Tipo de Vehiculo

---
+++ Traking.Vehiculos/src/Traking.Vehiculos.Domain/Enums/TipoVehiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traking.Vehiculos.Domain.Enums
+{
+    #region Enumaración TipoVehiculo
+
+    public enum TipoVehiculo
+    {
+        Urbano,
+        Sedan,
+        Berlina,
+        Hatchback,
+        Descapotable,
+        Coupe,
+        Deportivo,
+        Monovolumen,
+        Todoterreno,
+        Suv,
+        Roadster,
+        Pickup,
+        Furgoneta
+    }
+
+    #endregion
+
+}
