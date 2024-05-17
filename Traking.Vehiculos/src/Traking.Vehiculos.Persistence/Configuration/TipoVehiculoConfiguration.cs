38bd98c5e8691dfb5c9d2aec6e2751f50b42bb31
Chalo Mata <mauriciomata1997@hotmail.com>
2024-05-17 17:29:15 -0600

    Se agrega clase de referencia de entidades y BD local

---
+++ Traking.Vehiculos/src/Traking.Vehiculos.Persistence/Configuration/TipoVehiculoConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Traking.Vehiculos.Domain.Entities.TipoVehiculo;
+
+namespace Traking.Vehiculos.Persistence.Configuration
+{
+    public class TipoVehiculoConfiguration
+    {
+        public TipoVehiculoConfiguration(EntityTypeBuilder<TipoVehiculoEntity> entityBuilder)
+        {
+        }
+    }
+}
