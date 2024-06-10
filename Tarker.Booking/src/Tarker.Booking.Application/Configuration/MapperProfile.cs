21ae3a13059f81c6d7a3b940d18e13598b1231ba
Chalo Mata <mauriciomata1997@hotmail.com>
2024-06-10 14:49:12 -0600

    Integrar AutoMapper con Inyección de dependencias

---
+++ Tarker.Booking/src/Tarker.Booking.Application/Configuration/MapperProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarker.Booking.Application.Configuration
+{
+    /// <summary>
+    /// Clase de configuración para Profile con AutoMapper.
+    /// </summary>
+    /// <remarks>
+    /// Autor: Gonzalo Mata
+    /// Fecha: 10/06/2024
+    /// </remarks>
+    public class MapperProfile : Profile
+    {
+        /// <summary>
+        /// Constructor para mapaer los objetos que se vayan necesitando.
+        /// </summary>
+        /// <remarks>
+        /// Autor: Gonzalo Mata
+        /// Fecha: 10/06/2024
+        /// </remarks>
+        public MapperProfile() { 
+        
+        }
+    }
+}
--- Tarker.Booking/src/Tarker.Booking.Application/DependencyInjectionService.cs
+++ Tarker.Booking/src/Tarker.Booking.Application/DependencyInjectionService.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tarker.Booking.Application.Configuration;
 
 namespace Tarker.Booking.Application
 {
@@ -17,7 +19,7 @@ namespace Tarker.Booking.Application
     public static class DependencyInjectionService
     {
         /// <summary>
-        /// Método static para retornar IServiceCollection
+        /// Método static para retornar IServiceCollection.
         /// </summary>
         /// <param name="services"></param>
         /// <returns></returns>
@@ -27,6 +29,19 @@ namespace Tarker.Booking.Application
         /// </remarks>
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            // 1. Objeto para la configuración del mapeo con AutoMapper.
+            var mapper = new MapperConfiguration(config =>
+            {
+                // 2. Es una clase que define las reglas de mapeo entre los objetos.
+                // 3. AddProfile añade estas reglas al mapeador.
+                config.AddProfile(new MapperProfile());
+            });
+
+            // 5. Registro del mapeador como un servicio singleton.
+            // 6 .Un servicio singleton es una instancia única que se crea una vez y se reutiliza en toda la aplicación.
+            // 7. mapper.CreateMapper() crea una instancia de IMapper utilizando la configuración definida anteriormente.
+            services.AddSingleton(mapper.CreateMapper());
+
             return services;
         }
     }
